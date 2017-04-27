b943c33a7b04ac5a6b26bcc52c94ba9ab4f9567f
Ethan Harlig <ethan.harlig@gmail.com>
2017-04-26 17:34:40 -0700

    GetAll added to get all beers for each quantity

---
+++ src/main/java/edu/calpoly/eharlig/budget_brews/services/GetAll.java
@@ -0,0 +1,67 @@
+package edu.calpoly.eharlig.budget_brews.services;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ScanRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import edu.calpoly.eharlig.budget_brews.models.Beer;
+import edu.calpoly.eharlig.budget_brews.util.Credentials;
+
+public class GetAll implements RequestHandler<Object, List<ArrayList<Beer>>> {
+	// these are commented so that travis can pass
+	// need to think of a way to keep credentials here but have travis pass
+	private static String AWS_KEY = new Credentials().getAwsAccessKey();
+	private static String SECRET_KEY = new Credentials().getAwsSecretKey();
+//	 private static String AWS_KEY;
+//	 private static String SECRET_KEY;
+
+	private static AmazonDynamoDBClient client = new AmazonDynamoDBClient(new BasicAWSCredentials(AWS_KEY, SECRET_KEY))
+			.withRegion(Regions.US_WEST_2);
+
+	public List<ArrayList<Beer>> handleRequest(Object request, Context context) {
+		List<ArrayList<Beer>> beers = new ArrayList<ArrayList<Beer>>();
+		beers.add(getAllQuantity(12));
+		beers.add(getAllQuantity(30));
+
+		return beers;
+	}
+
+	private ArrayList<Beer> getAllQuantity(int quantity) {
+		ScanRequest scanRequest = new ScanRequest().withTableName("beer-" + quantity);
+
+		ScanResult result = client.scan(scanRequest);
+		
+		ArrayList<Beer> allBeers = new ArrayList<Beer>();
+
+		for (Map<String, AttributeValue> item : result.getItems()) {
+			Beer current = new Beer();
+			current.setName(item.get("name").getS());
+			current.setPrice(Double.parseDouble(item.get("price").getN()));
+			current.setStoreName(item.get("storeName").getS());
+			current.setTimestamp(Long.parseLong(item.get("timestamp").getN()));
+			current.setQuantity(quantity);
+			
+			allBeers.add(current);
+		}
+		
+		Collections.sort(allBeers, new Comparator<Beer>() {
+			public int compare(Beer b1, Beer b2) {
+				return Double.compare(b1.getPrice(), b2.getPrice());
+			}
+		});
+
+		return allBeers;
+	}
+
+}
