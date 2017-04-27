486a6fd10829ed2746b15a73072ed81fe7305e02
Ethan Harlig <ethan.harlig@gmail.com>
2017-04-26 17:35:12 -0700

    Split tables to multiple tables by quantity. Added button

--- website/res/js/home.js
+++ website/res/js/home.js
@@ -5,26 +5,35 @@ $(document).ready(function () {
 
 function getCheapest() {
     $.ajax({
-        url: 'https://s7pc06oh92.execute-api.us-west-2.amazonaws.com/test/cheapest-price?',
+        url: 'https://s7pc06oh92.execute-api.us-west-2.amazonaws.com/test/all-beers?',
         type: 'GET',
         success: function (response) {
             if (response == null) {
                 alert("Dang");
             } else {
-                var brewsTableBody = $("#brews-table tbody");
-                response.forEach(function (entry) {
-                    var tr = "<tr>";
-                    tr += "<td>$" + entry.price + "</td>";
-                    tr += "<td>" + entry.quantity + " beers</td>";
-                    tr += "<td>" + entry.name + "</td>";
-                    tr += "<td>" + entry.storeName + "</td>";
-                    tr += "<td>" + moment(new Date(0).setUTCMilliseconds(entry.timestamp)).fromNow() + "</td>";
-                    tr += "</tr>";
-                    brewsTableBody.append(tr);
+                response.forEach(function (oneQuantity) {
+                    var quantity = oneQuantity[0].quantity;
+                    oneQuantity.forEach(function (entry) {
+                        var tr = "<tr>";
+                        tr += "<td>$" + entry.price + "</td>";
+                        tr += "<td>" + entry.name + "</td>";
+                        tr += "<td>" + entry.storeName + "</td>";
+                        tr += "<td>" + moment(new Date(0).setUTCMilliseconds(entry.timestamp)).fromNow() + "</td>";
+                        tr += "</tr>";
+                        $("#brews-table-" + entry.quantity + " tbody").append(tr);
+                    });
+                    toggleButton(quantity);
                 });
             }
-        },
-        complete: function () {}
+        }
     });
 
+}
+
+function toggleButton(quantity) {
+    $("#brews-table-" + quantity + " > tbody > tr").hide().slice(0, 3).show();
+    $("#show-" + quantity).on("click", function () {
+        $("tbody > tr", $(this).prev()).show();
+        $("#show-" + quantity).html("Show fewer");
+    });
 }
