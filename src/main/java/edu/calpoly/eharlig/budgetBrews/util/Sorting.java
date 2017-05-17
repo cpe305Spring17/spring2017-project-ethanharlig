1568e077a02f68ab41ae18f0782e2d81282dda5f
Ethan Harlig <ethan.harlig@gmail.com>
2017-05-17 13:18:59 -0700

    More code smell resolving

--- src/main/java/edu/calpoly/eharlig/budgetBrews/util/Sorting.java
+++ src/main/java/edu/calpoly/eharlig/budgetBrews/util/Sorting.java
@@ -7,8 +7,9 @@ import java.util.List;
 import edu.calpoly.eharlig.budgetbrews.models.Beer;
 
 public class Sorting {
+  private Sorting() {}
+  
   public static List<Beer> sort(List<Beer> beers) {
-
     Collections.sort(beers, new Comparator<Beer>() {
       public int compare(Beer b1, Beer b2) {
         return Double.compare(b1.getPrice(), b2.getPrice());
