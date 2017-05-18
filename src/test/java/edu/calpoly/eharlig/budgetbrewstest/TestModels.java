bd58d37ae4d4c6c83c575a6a3ca0a56b4e9231ef
Ethan Harlig <ethan.harlig@gmail.com>
2017-05-17 18:39:00 -0700

    Added tests to sonarqube

--- src/test/java/edu/calpoly/eharlig/budgetbrewstest/TestModels.java
+++ src/test/java/edu/calpoly/eharlig/budgetbrewstest/TestModels.java
@@ -9,74 +9,84 @@ import junit.framework.TestCase;
 
 public class TestModels extends TestCase {
   Beer beer = new Beer();
-  
-  public void testConstructor() {
+  Subscription sub = new Subscription();
+  User user = new User();
 
+  public void testBeerConstructor() {
     assertTrue(beer.getHistory().equals(new ArrayList<>()));
   }
+
   public void testDownvotes() {
-    
     beer.setDownvotes(5);
     assertTrue(beer.getDownvotes() == 5);
   }
+
   public void testHistory() {
-    
     beer.setHistory(new ArrayList<>());
     assertTrue(beer.getHistory().equals(new ArrayList<>()));
   }
+
   public void testName() {
-    
     beer.setName("myBeer");
     assertTrue(beer.getName().equals("myBeer"));
   }
+
   public void testObservers() {
     beer.setObservers(new ArrayList<>());
     assertTrue(beer.getObservers().equals(new ArrayList<>()));
   }
-    
+
   public void testPrice() {
     beer.setPrice(24.31);
     assertTrue(Double.compare(beer.getPrice(), 24.31) == 0);
   }
+
   public void testQuantity() {
     beer.setQuantity(40);
     assertTrue(beer.getQuantity() == 40);
   }
+
   public void testStoreName() {
     beer.setStoreName("myStoreName");
     assertTrue(beer.getStoreName().equals("myStoreName"));
   }
+
   public void testTimestamp() {
     beer.setTimestamp(1234567890);
     assertTrue(beer.getTimestamp() == 1234567890);
   }
+
   public void testUpvotes() {
     beer.setUpvotes(55);
     assertTrue(beer.getUpvotes() == 55);
   }
-  
-  public void testSubscription() {
-    Subscription sub = new Subscription();
-    
+
+  public void testSubBeerName() {
     sub.setBeerName("myBeer");
     assertTrue(sub.getBeerName().equals("myBeer"));
-    
+  }
+  
+  public void testSubEmail() {
     sub.setEmail("testemail123@email.com");
     assertTrue(sub.getEmail().equals("testemail123@email.com"));
-    
+  }
+  
+  public void testSubQuantity() {
     sub.setQuantity(36);
     assertTrue(sub.getQuantity() == 36);
   }
-  
-  public void testUser() {
-    User user = new User();
-    
+
+  public void testUserEmail() {
     user.setEmail("testemail321@email.com");
     assertTrue(user.getEmail().equals("testemail321@email.com"));
-    
+  }
+  
+  public void testUserPassword() {
     user.setPassword("password");
     assertTrue(user.getPassword().equals("password"));
-    
+  }
+  
+  public void testUserUsername() {
     user.setUsername("myusername");
     assertTrue(user.getUsername().equals("myusername"));
   }
