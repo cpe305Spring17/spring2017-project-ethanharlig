df6f7b4f7d33d1f1feb82f97f4df236ca251176d
Ethan Harlig <ethan.harlig@gmail.com>
2017-06-08 15:47:11 -0700

    Removed code smell

--- src/main/java/edu/calpoly/eharlig/budgetbrews/util/Vote.java
+++ src/main/java/edu/calpoly/eharlig/budgetbrews/util/Vote.java
@@ -9,7 +9,7 @@ import edu.calpoly.eharlig.budgetbrews.dataaccess.DBAccess;
 import edu.calpoly.eharlig.budgetbrews.models.Subscription;
 
 public class Vote {
-  public Vote() {}
+  private Vote() {}
 
   public static void vote(Subscription sub, String voteType) {
     Table beerTable = DBAccess.getTable("beer-" + sub.getQuantity());
